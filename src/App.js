4111fd0880493a4cd8702d691fb9a14bd11aabf7
AshwiniKori <ashwinibkori1995@gmail.com>
2023-06-13 16:49:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import  Fonts  from 'react-remixicon';
+import 'remixicon/fonts/remixicon.css'
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return <Layout />;
+}
+
+export default App;
